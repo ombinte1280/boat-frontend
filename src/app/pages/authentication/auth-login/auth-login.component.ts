a51f62a6a9e74269317278573a6e149f938da549
mbint <mbinte@gmail.com>
2025-04-23 11:02:32 +0200

    :sparkles: Implementation of login feature

--- src/app/pages/authentication/auth-login/auth-login.component.ts
+++ src/app/pages/authentication/auth-login/auth-login.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthenticationService} from '../services/authentication.service';
+import {Router} from '@angular/router';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {LoginModel} from '../models/login.model';
+import {UserModel} from '../models/user.model';
+import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-auth-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatButton
+  ],
   templateUrl: './auth-login.component.html',
   styleUrl: './auth-login.component.scss',
   standalone: true
 })
 export class AuthLoginComponent {
 
+  loginForm: FormGroup;
+  validationErrors:any = []
+
+  constructor(private fb: FormBuilder, private authService: AuthenticationService
+    , private router: Router) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if(this.loginForm.valid) {
+      const formValue = this.loginForm.value;
+      const loginModel: LoginModel = {
+        username: formValue.username,
+        password: formValue.password
+      }
+
+      this.authService.login(loginModel).subscribe({
+        next: (user: UserModel) => {
+          localStorage.setItem('token', user.token);
+          localStorage.setItem('user', user.firstname + ' ' + user.lastname);
+          this.router.navigateByUrl('/boat-list');
+        },
+        error: err => {
+          console.log('Login failed:', err);
+          this.validationErrors = err.response ? err.response.data.errors : err;
+        }
+      });
+    }
+  }
 }
---
+++ src/app/pages/authentication/services/authentication.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {environment} from '../../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {LoginModel} from '../models/login.model';
+import {catchError, Observable, throwError} from 'rxjs';
+import {UserModel} from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  private authBaseUrl = environment.baseUrl + '/users';
+
+  constructor(private http: HttpClient) {
+  }
+
+  login(login: LoginModel): Observable<UserModel> {
+    return this.http.post<UserModel>(this.authBaseUrl +'/login', login).pipe(
+      catchError(error => {
+        console.error('Login API error', error);
+        return throwError(() => error);
+      })
+    );
+  }
+
+}
